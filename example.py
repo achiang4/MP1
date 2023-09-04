6b5eae02ea138e778df4b008a207ccdbe09d3ed6
achiang4 <kingveedee@gmail.com>
2023-09-04 17:36:53 -0500

    MP1_Solution

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ def tokens_lowercase(doc):
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
     '''Place your code here'''
 
-    tok = metapy.analyzers.ICUTokenizer()  #intialize
+    tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)  #intialize
 
     tok = metapy.analyzers.LowercaseFilter(tok)  #lowercase everything
 
